597870c27606220ec7156d8dc6367a8aa2ff70bc
Nala <24021544@vnu.edu.vn>
2026-05-27 22:41:00 +0700

    somrething changed

--- customer_loan/loan_origination_system.py
+++ customer_loan/loan_origination_system.py
@@ -9,16 +9,16 @@ class LoanOriginationSystem:
     def check_valid_input(self):
         """Checks if the customer's input is valid."""
         if not isinstance(self.customer.age, int) or self.customer.age < 18 or self.customer.age > 65:
-            raise ValueError("Invalid Input: Age must be an integer between 18 and 65.")
+            raise ValueError("Invalid Input")
         
         if not isinstance(self.customer.income, (int, float)) or self.customer.income < 5 or self.customer.income > 500:
-            raise ValueError("Invalid Input: Income must be a number between 5 and 500.")
+            raise ValueError("Invalid Input")
         
         if not isinstance(self.customer.credit_score, int) or self.customer.credit_score < 300 or self.customer.credit_score > 850:
-            raise ValueError("Invalid Input: Credit score must be an integer between 300 and 850.")
+            raise ValueError("Invalid Input")
         
         if not isinstance(self.customer.employment, str) or self.customer.employment not in ['C', 'F']:
-            raise ValueError("Invalid Input: Employment must be 'C' for contract or 'F' for freelance.")
+            raise ValueError("Invalid Input")
         
         print("All inputs are valid.")
         return True
